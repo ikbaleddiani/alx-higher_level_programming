c8323b9f50ad5bd6e6f05463d17b1fc61832024b
Ikbal Eddiani <eddianiiqbal@gmail.com>
2023-06-06 19:29:56 -0700

    Fizz Buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for number in range(1, 101):
-    if number % 15 == 0:
+        if number % 15 == 0:
         print("FizzBuzz", end=" ")
     elif number % 3 == 0:
         print("Fizz", end=" ")
