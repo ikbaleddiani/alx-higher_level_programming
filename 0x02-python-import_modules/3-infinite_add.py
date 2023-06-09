a8197943ce34f20e8d70c8899429e8baaf644781
EDDIANI IKBAL <eddianiiqbal@gmail.com>
2023-06-08 17:08:47 -0700

    program that prints the result of the addition of all arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if _name_ == "_main_":
+    from sys import argv
+    args = len(argv)
+    if args == 1
+        print(0)
+    adds = 0
+    for i in range(1, args):
+        adds += int(argv[i])
+        print("{0:d}".format(adds))
