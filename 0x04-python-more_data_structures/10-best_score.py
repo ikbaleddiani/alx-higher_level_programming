92aa3789778a236458a8718ac1bf23034147cfdc
EDDIANI IKBAL <eddianiiqbal@gmail.com>
2023-06-18 17:13:38 -0700

    10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    best_key = ""
+    best_score = 0
+    for key in a_dictionary:
+        f best_score < a_dictionary[key]:
+            best_score = a_dictionary[key]
+            best_key = key
+    return best_key 
