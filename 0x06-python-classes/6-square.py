b005a52ca704afd81972b77e7f8c8512a4080798
Ikbal Eddiani <eddianiiqbal@gmail.com>
2023-06-29 17:42:06 -0700

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -8,8 +8,8 @@ class Square:
     "Initializes a square with attriute size"
 
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
