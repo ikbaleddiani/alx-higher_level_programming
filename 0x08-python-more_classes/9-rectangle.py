eaa57b3093a7184db3d397f6736cd0478b847ee1
Ikbal Eddiani <eddianiiqbal@gmail.com>
2023-07-05 17:39:32 -0700

    A square is a rectanglewq!

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -69,6 +69,7 @@ class Rectangle:
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
 
+    @staticmethod:
     def bigger_or_equal(rect_1, rect_2):
         if type(rect_1) is not Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
