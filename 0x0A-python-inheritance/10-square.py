ff9106a9303c391ba5b1d352bc620387afb45da9
Ikbal Eddiani <eddianiiqbal@gmail.com>
2023-07-11 04:36:36 -0700

    Square #1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"rectangle module"
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    "class of square"
+
+    def __init__(self, size):
+        "initialisation of squere"
+
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+
+    def area(self):
+        """"returns the area of the square"""
+
+        return self.__size ** 2
