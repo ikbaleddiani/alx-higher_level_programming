22d698f95885a0100b8319e1523c4cf93b363cd9
Ikbal Eddiani <eddianiiqbal@gmail.com>
2023-07-11 16:17:39 -0700

    Student to JSON

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Defiens a student class that have to_json public method"""
+
+
+
+class Student:
+    """student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initalizes a student object"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
