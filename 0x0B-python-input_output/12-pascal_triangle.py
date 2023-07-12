e96c89511b3bc245f7dc3486adc7cfd6620c2441
Ikbal Eddiani <eddianiiqbal@gmail.com>
2023-07-11 18:29:57 -0700

    Pascal's Triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""defines a function pascal_triangle"""
+
+def pascal_triangle(n):
+    """prtint the pascal triangle"""
+    matrix = []
+    for i in range(n):
+        num = 11 ** i
+        s = str(num)
+        list1 = []
+        for c in s:
+            list1.append(int(c))
+        matrix.append(list1)
+    return matrix
