5e2ebf677874193726cc7ec004e440d6eb51f207
Ikbal Eddiani <eddianiiqbal@gmail.com>
2023-07-11 17:00:44 -0700

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""student to json"""
+
+class Student:
+    """student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initalizes a student object"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns a json representatiion of the object"""
+
+        if attrs is None:
+            return self.__dict__
+
+        else:
+            jsn_dic = {}
+            for ite in attrs:
+                for k in self.__dict__:
+                    if k == ite:
+                        jsn_dic[k] = self.__dict__[k]
+            return jsn_dic
