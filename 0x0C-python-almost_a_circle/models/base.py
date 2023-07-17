3976f4297154399ba6ef907785ca7c19e43ec666
Ikbal Eddiani <eddianiiqbal@gmail.com>
2023-07-17 14:56:30 -0700

    folders

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""Class Module"""
+
+
+
+class Base:
+
+    """
+    Attributes of class Base:
+    _nb_objects: number of objects created
+    id: id of object
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """initiation method"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+
