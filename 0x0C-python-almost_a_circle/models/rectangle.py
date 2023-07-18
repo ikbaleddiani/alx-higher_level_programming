4995f7267654e77375216e9247482c265aa1ccf4
Ikbal Eddiani <eddianiiqbal@gmail.com>
2023-07-17 17:11:13 -0700

    Display #0and __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -85,3 +85,11 @@ class Rectangle(Base):
         for row in range(self.height):
             print("{}{}".format(" " * self.x, "#" * self.width))
 
+        def __str__(self):
+            """update: returns [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
+
+            return "[{}] ({}) {}/{} - {}/{}".format(self.__class__.__name__,
+                                                    self.id, self.__x,
+                                                    self.__y,
+                                                    self.__width, self.__height
+                                                    )
