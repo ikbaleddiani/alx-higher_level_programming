748df950e18832bb15b80a0820607ac1e78533cc
IkbalEddiani <ikbaleddiani@gmail.com>
2023-09-22 17:53:32 -0700

    First state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""List all records in the table staes from a database"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = sessionmaker(bind=engine)
+    session = session()
+    state = session.query(State).order_by(State.id).first()
+    print(f"{state.id}: {state.name}")
+    session.close()
