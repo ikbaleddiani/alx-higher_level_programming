65d3f50c0c43badc845eeb139623a2f2912f98e0
IkbalEddiani <ikbaleddiani@gmail.com>
2023-09-22 18:02:38 -0700

    Print a state with name passed as argument

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""List all records in the table staes from a database"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = sessionmaker(bind=engine)
+    session = session()
+    state = session.query(State).filter(
+            State.name.like(sys.argv[4])).order_by(State.id).first()
+    if state:
+        print(f"{state.id}")
+    else:
+        print("Not found")
+    session.close()
